d1d9355f8e084e317fd13dc57386f3014cf093c6
ocs-133305 <ocs_takeda@yahoo.co.jp>
2014-06-17 22:45:09 +0900

    複製テスト

--- GitTest/src/Hello.java
+++ GitTest/src/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
 	public static void main(String[] args) {
 
 		System.out.println("Hello. I am Penguin.");
-		System.out.println("コミットに成功したようです。");
+		System.out.println("コミットに成功したようです?");
+		System.out.println();
 
 	}
 
